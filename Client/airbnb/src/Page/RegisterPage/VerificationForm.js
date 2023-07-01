0873e9e5c658b44a2b0248ddf51488982290fe62
NguyenTruongGiang <19130061@st.hcmuaf.edu.com>
2023-07-01 19:05:36 +0700

    config gg map

---
+++ Client/airbnb/src/Page/RegisterPage/VerificationForm.js
@@ -0,0 +1,12 @@
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+const VerificationForm = () => {
+  
+
+  return (
+    <div>verify form mail </div>
+  );
+};
+
+export default VerificationForm;
--- Client/airbnb/src/Router/Routers.js
+++ Client/airbnb/src/Router/Routers.js
@@ -11,6 +11,7 @@ import HouseManager from '../Page/Manager/HouseManager/HouseManager'
 import AddHouseManager from '../Page/Manager/HouseManager/AddHouseManager/AddHouseManager'
 import BookingCalendar from '../Page/Manager/BookingCalendar'
 import OrderPage from '../Page/OrderPage/OrderPage'
+import Map from '../Page/Map'
 
 export default function Routers() {
 
@@ -21,6 +22,7 @@ export default function Routers() {
                 <Route path='/login' element={<LoginPage/>} /> 
                 <Route path='/register' element={<RegisterPage/>} /> 
                 <Route path='/*' element={<NotFoundPage/>} /> 
+                <Route path='/map' element={<Map/>} /> 
                 <Route path='/' element = {<MainLayout/>}>
                     <Route path='/' element={<HomePage/>} /> 
                     <Route path='/order' element={<OrderPage/>} /> 
