5a6c812511da3a718b829920be8b6d95f10d00d8
CallMeHieu <motnghindong2704@gmail.com>
2023-07-06 13:03:53 +0700

    update status user

--- Sever/airbnb/src/main/java/com/techpower/airbnb/service/impl/UserService.java
+++ Sever/airbnb/src/main/java/com/techpower/airbnb/service/impl/UserService.java
@@ -1,9 +1,13 @@
 package com.techpower.airbnb.service.impl;
 
+import com.techpower.airbnb.constant.Status;
 import com.techpower.airbnb.converter.OrderConverter;
 import com.techpower.airbnb.converter.RoomConverter;
+import com.techpower.airbnb.converter.UserDTOMapper;
 import com.techpower.airbnb.dto.OrderDTO;
 import com.techpower.airbnb.dto.RoomDTO;
+import com.techpower.airbnb.dto.UserDTO;
+import com.techpower.airbnb.entity.UserEntity;
 import com.techpower.airbnb.repository.*;
 import com.techpower.airbnb.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,11 @@ public class UserService implements IUserService {
     private OrderRepository orderRepository;
     @Autowired
     private OrderConverter orderConverter;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserDTOMapper userDTOMapper;
+
     @Override
     public List<OrderDTO> findAllOrders(long idUser) {
         return orderConverter.mapperTOList(orderRepository.findAllByUserId(idUser));
@@ -29,5 +38,12 @@ public class UserService implements IUserService {
     @Override
     public List<RoomDTO> findAllRooms(long idUser) {
         return roomConverter.toDTOs(roomRepository.findAllByUserId(idUser));
+    }
+
+    @Override
+    public UserDTO updateStatus(Status status, long idUser) {
+        UserEntity userEntity = userRepository.findOneById(idUser);
+        userEntity.setStatus(status);
+        return userDTOMapper.apply(userRepository.save(userEntity));
     }
 }
