77c02c35b8df1d2e65885d794aeae4f0c638ce6c
CallMeHieu <motnghindong2704@gmail.com>
2023-08-11 10:47:03 +0700

    api user

--- Sever/airbnb/src/main/java/com/techpower/airbnb/repository/UserRepository.java
+++ Sever/airbnb/src/main/java/com/techpower/airbnb/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.techpower.airbnb.repository;
 
+import com.techpower.airbnb.constant.Role;
 import com.techpower.airbnb.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -7,11 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findOneById(long idUser);
+
+    List<UserEntity> findAllByRole(Role role);
 
     UserEntity findOneByEmail(String email);
 
